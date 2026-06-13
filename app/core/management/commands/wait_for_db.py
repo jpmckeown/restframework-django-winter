b8d07c482f2cb3e8dacea5eb8f536e864f479bd3
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-13 19:55:10 +0100

    begin mgmt command empty before testing

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""wait for database to be available - a Django command"""
+
+from django.core.management.base import BaseCommand
+
+
+# generic empty command template
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
