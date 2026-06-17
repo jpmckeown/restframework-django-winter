06f87c2d509d8f79a84d1799f10b692510fdae20
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-17 12:35:54 +0100

    TDD failing=test for new user email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    def test_make_user_with_email(self):
+    def test_create_user_with_email(self):
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -15,3 +15,12 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['Test2A@example.COM', 'Test2A@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
