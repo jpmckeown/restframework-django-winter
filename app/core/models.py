7c194a3fc73cfc0750e8ac75472bc63454f89c6f
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-17 07:26:19 +0100

    User model based on baseUser"

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,17 @@
 """Database models"""
 
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
