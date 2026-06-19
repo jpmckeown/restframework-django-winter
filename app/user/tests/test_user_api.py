5663a46a2426d66a64cd6343c1957d4059adbbab
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-19 21:28:32 +0100

    test unauth usage of user page: registering new user, TDD

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """valid unauthenticated usage e.g. registering new user"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user_works(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(email=payload['email'])
+        self.assertTrue(user.check_password(payload['password']))
+
+        self.assertNotIn('password', res.data)
