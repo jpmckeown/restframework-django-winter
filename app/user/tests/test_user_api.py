280eee93805792af22e39a83531c81a3421aa503
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-20 10:07:05 +0100

    test: if user_exists with_same email create-user fails

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -31,3 +31,14 @@ class PublicUserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password']))
 
         self.assertNotIn('password', res.data)
+
+    def test_user_with_email_already_exists(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testpass123',
+            'name': 'Test Name',
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
