39c1b4ca88acca49effb147f2332a15e1843464d
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-06-21 08:54:26 +0100

    testing user-making by public API, dup email/username; password length

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,7 +32,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn('password', res.data)
 
-    def test_user_with_email_already_exists(self):
+    def test_user_with_email_already_exists_error(self):
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
@@ -42,3 +42,18 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def password_too_short_error(self):
+        payload = {
+            'email': 'test@example.com',
+            'password': 'T12',
+            'name': 't',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        # should return error status
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # should not have created new user
+        user_exists = get_user_model().objects.filter(email=payload['email']).exists()
+        self.assertFalse(user_exists)
